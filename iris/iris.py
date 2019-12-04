2073c2973a70cc107a003c4765d05423fccf9a99
Rafael <rafael.sterzinger@gmx.at>
2019-12-04 02:16:28 +0100

    add plots and basic models to iris

--- iris/iris.py
+++ iris/iris.py
@@ -1,3 +1,63 @@
-#%%
+# %%
 import pandas as pd
-data = pd.read_csv("iris/dataset/iris.data")
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+
+plt.rcParams["patch.force_edgecolor"] = True
+
+data = pd.read_csv('iris/dataset/iris.data', names=['sep_length', 'sep_width', 'pet_length', 'pet_width', 'type'])
+numeric = ['sep_length', 'sep_width', 'pet_length', 'pet_width']
+target = 'type'
+
+# %%
+data.describe()
+correlation_matrix = data[numeric].corr().round(2)
+sns.heatmap(correlation_matrix, linewidths=1, annot=True)
+# fix for cut off boxes
+b, t = plt.ylim()  # discover the values for bottom and top
+b += 0.5  # Add 0.5 to the bottom
+t -= 0.5  # Subtract 0.5 from the top
+plt.ylim(b, t)  # update the ylim(bottom, top) values
+plt.show()
+
+# %%
+#Sepal
+fig = data[data['type'] == 'Iris-setosa'].plot(kind='scatter', x='sep_length', y='sep_width', color='red',
+                                               label='Setosa')
+data[data['type'] == 'Iris-versicolor'].plot(kind='scatter', x='sep_length', y='sep_width', color='blue',
+                                             label='Versicolor', ax=fig)
+data[data['type'] == 'Iris-virginica'].plot(kind='scatter', x='sep_length', y='sep_width', color='green',
+                                            label='Virginica', ax=fig)
+fig.set_xlabel("Sepal Length")
+fig.set_ylabel("Sepal Width")
+fig.set_title("Sepal: Length vs. Width")
+fig = plt.gcf()
+fig.set_size_inches(10, 6)
+plt.show()
+
+#Petal
+fig = data[data['type'] == 'Iris-setosa'].plot(kind='scatter', x='pet_length', y='pet_width', color='red',
+                                               label='Setosa')
+data[data['type'] == 'Iris-versicolor'].plot(kind='scatter', x='pet_length', y='pet_width', color='blue',
+                                             label='Versicolor', ax=fig)
+data[data['type'] == 'Iris-virginica'].plot(kind='scatter', x='pet_length', y='pet_width', color='green',
+                                            label='Virginica', ax=fig)
+fig.set_xlabel("Petal Length")
+fig.set_ylabel("Petal Width")
+fig.set_title("Petal: Length vs. Width")
+fig = plt.gcf()
+fig.set_size_inches(10, 6)
+plt.show()
+
+#%%
+
+scores = cross_val_score(KNeighborsClassifier(), data[numeric], data[target], cv=10).mean()
+
+scores = cross_val_score(DecisionTreeClassifier(), data[numeric], data[target], cv=10).mean()
+
+scores = cross_val_score(MLPClassifier(max_iter=1000), data[numeric], data[target], cv=10).mean()
+
