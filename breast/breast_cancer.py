76c18cb0fa079303add7bcb098fd0e784893f6ee
Rafael <rafael.sterzinger@gmx.at>
2019-12-05 13:13:21 +0100

    change breast cancer mlp

--- breast/breast_cancer.py
+++ breast/breast_cancer.py
@@ -14,15 +14,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
-from sklearn.preprocessing import LabelEncoder
 
-plt.rcParams["patch.force_edgecolor"] = True
 
 train = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.lrn.csv')
 train.columns = train.columns.str.strip()
 
 # %% Pre-Processing
-# y = LabelEncoder().fit_transform(train['class'])
 y = train['class']
 X = train.drop(['ID', 'class'], axis=1)
 
@@ -45,16 +42,16 @@ mlp = cross_val_score(MLPClassifier(
 X_scale = min_max_scaler.transform(X)
 mlp_activation = ['logistic', 'relu']
 mlp_max_iter = range(1000, 5000, 1000)
-mlp_alpha = [1e-3, 0.01, 0.1]
+mlp_alpha = [0.0001, 0.001, 0.01, 0.1]
 
 mlp = GridSearchCV(MLPClassifier(solver='adam'), param_grid=[{'activation': mlp_activation},
                                                              {'alpha': mlp_alpha}, {'max_iter': mlp_max_iter}], cv=10)
 
 mlp.fit(X_scale, y)
-# max_iter=1000,alpha=0.001,activation=relu
+# max_iter=1000,alpha=0.0001,activation=relu
 best_params = mlp.best_params_
 
-# %%
+# %% Kaggle Score 0.97647
 X_scale = min_max_scaler.transform(X)
 test = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.tes.csv').drop('ID', axis=1)
 test.columns = test.columns.str.strip()
@@ -62,8 +59,8 @@ sol = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.sol.ex.csv')
 
 mlp = MLPClassifier(
     max_iter=1000,
-    alpha=0.1,
-    activation='logistic',
+    alpha=0.0001,
+    activation='relu',
     solver='adam',
 )
 mlp.fit(X_scale, y)
