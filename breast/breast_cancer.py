1c57f343bf88ddbc9c7deb523cea43ce32414434
Stippler <christian.stippel@gmail.com>
2019-12-15 21:23:04 +0100

    finish breast-cancer.tex

--- breast/breast_cancer.py
+++ breast/breast_cancer.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
 from sklearn import preprocessing
-from sklearn.model_selection import cross_val_score,cross_validate
+from sklearn.model_selection import cross_val_score, cross_validate
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -18,7 +18,7 @@ from sklearn.feature_selection import chi2
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import make_scorer,accuracy_score,precision_score, recall_score, f1_score
+from sklearn.metrics import make_scorer, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import RFECV
 from scipy.stats import uniform, truncnorm, randint
@@ -30,7 +30,7 @@ random = 47
 scoring = {'Accuracy': make_scorer(accuracy_score), 'Precision': make_scorer(precision_score, average='macro'),
            'Recall': make_scorer(recall_score, average='macro'), 'F1': make_scorer(f1_score, average='macro')}
 
-train = pd.read_csv('dataset/breast-cancer-diagnostic.shuf.lrn.csv')
+train = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.lrn.csv')
 train.columns = train.columns.str.strip()
 
 # %% Pre-Processing
@@ -61,6 +61,13 @@ plt.xticks(rotation=40)
 plt.savefig("plots/violinplot.png")
 plt.show()
 
+# %% Boxplot
+plt.figure(figsize=(10, 10))
+sns.boxplot(x="features", y="value", hue="class", data=data)
+plt.xticks(rotation=90)
+plt.savefig("plots/boxplot.png")
+plt.show()
+
 # %% violineplot normal transformed
 data_dia = y
 data = (X - X.mean()) / (X.std())
@@ -73,13 +80,6 @@ plt.figure(figsize=(10, 10))
 sns.violinplot(x="features", y="value", hue="class", data=data, split=True, inner="quart")
 plt.xticks(rotation=90)
 plt.savefig("plots/violineplot_normaltransformed.png")
-plt.show()
-
-# %% Boxplot
-plt.figure(figsize=(10, 10))
-sns.boxplot(x="features", y="value", hue="class", data=data)
-plt.xticks(rotation=90)
-plt.savefig("plots/boxplot.png")
 plt.show()
 
 # %%
@@ -100,7 +100,7 @@ drop_list1 = ['perimeterMean', 'radiusMean', 'compactnessMean', 'concavePointsMe
               'concavePointsStdErr', 'textureWorst', 'areaWorst']
 X_best = X.drop(drop_list1, axis=1)  # do not modify x, we will use it later
 
-#%% Comparing Feature Selection
+# %% Comparing Feature Selection
 k = np.arange(1, 40)
 metric = ['euclidean', 'chebyshev', 'manhattan']
 
@@ -140,7 +140,7 @@ plt.plot(range(1, len(rf_ecv.grid_scores_) + 1), rf_ecv.grid_scores_)
 plt.savefig("plots/rf_feature_selection.png")
 plt.show()
 
-#%% KNN NP
+# %% KNN NP
 k = np.arange(1, 30)
 metric = ['euclidean', 'chebyshev', 'manhattan']
 
@@ -190,27 +190,27 @@ param_grid = {
 }
 
 rf = RandomizedSearchCV(RandomForestClassifier(random_state=random, criterion='gini'), param_grid, cv=4,
-                        n_jobs=-1, random_state=random, n_iter=100,verbose=True)
+                        n_jobs=-1, random_state=random, n_iter=100, verbose=True)
 rf.fit(X, y)
 
 print('Best Mean Score Approx Params', rf.best_score_, 'Model', rf.best_estimator_)
 
 # %% RF CV NP
 param_grid = {
-    'n_estimators': np.arange(1,50),
-    'max_features': [0.1,0.2,0.3,0.4,0.5],
+    'n_estimators': np.arange(1, 50),
+    'max_features': [0.1, 0.2, 0.3, 0.4, 0.5],
 }
 
 rf = GridSearchCV(RandomForestClassifier(random_state=random, min_samples_split=0.04), param_grid,
                   cv=5,
-                  n_jobs=-1,verbose=True)
+                  n_jobs=-1, verbose=True)
 rf.fit(X, y)
 print('Best Mean Score Without Preprocessing', rf.best_score_, 'Model', rf.best_estimator_)
 best_estimator = rf.best_estimator_
 
 rf_results = pd.DataFrame(rf.cv_results_)
-rf_results['param_max_features'] = list(map(lambda x: str(x*100) + ' %',rf_results['param_max_features']))
-sns.lineplot('param_n_estimators', 'mean_test_score','param_max_features',style='param_max_features', data=rf_results)
+rf_results['param_max_features'] = list(map(lambda x: str(x * 100) + ' %', rf_results['param_max_features']))
+sns.lineplot('param_n_estimators', 'mean_test_score', 'param_max_features', style='param_max_features', data=rf_results)
 plt.savefig("plots/rf_np_comparision.png")
 plt.show()
 
@@ -219,13 +219,13 @@ classifier_pipeline = make_pipeline(preprocessing.MinMaxScaler(),
                                     RandomForestClassifier(random_state=random, min_samples_split=0.04))
 
 param_grid = {
-    'randomforestclassifier__n_estimators': np.arange(1,50),
-    'randomforestclassifier__max_features': [0.1,0.2,0.3,0.4,0.5],
+    'randomforestclassifier__n_estimators': np.arange(1, 50),
+    'randomforestclassifier__max_features': [0.1, 0.2, 0.3, 0.4, 0.5],
 }
 
 rf = GridSearchCV(classifier_pipeline, param_grid,
                   cv=5,
-                  n_jobs=-1,verbose=True)
+                  n_jobs=-1, verbose=True)
 rf.fit(X, y)
 print('Best Mean Score With Preprocessing', rf.best_score_, 'Model', rf.best_estimator_)
 
@@ -242,22 +242,22 @@ print('Best Score Hold Out', best_estimator.score(X_test, y_test))
 
 # %% MLP CV approx params
 param_grid = {
-    'hidden_layer_sizes': [(15,15,15),(30, 15, 30), (15, 30, 15), (30, 30, 30)],
-    'activation': ['tanh', 'relu','logistic','identity'],
+    'hidden_layer_sizes': [(15, 15, 15), (30, 15, 30), (15, 30, 15), (30, 30, 30)],
+    'activation': ['tanh', 'relu', 'logistic', 'identity'],
     'solver': ['sgd', 'adam'],
     'learning_rate': ['constant', 'adaptive'],
     'alpha': [0.01, 0.001, 0.0001]
 }
 
 mlp = RandomizedSearchCV(MLPClassifier(max_iter=4000, random_state=random), param_grid, cv=5,
-                         n_jobs=-1, random_state=random,verbose=True)
+                         n_jobs=-1, random_state=random, verbose=True)
 
 mlp.fit(X, y)
 print('Best Mean Score Approx Params', mlp.best_score_, 'Model', mlp.best_estimator_)
 
 # %% MLP CV NP
 param_grid = {
-    'hidden_layer_sizes': [(15,15,15),(30, 15, 30), (15, 30, 15), (30, 30, 30)],
+    'hidden_layer_sizes': [(15, 15, 15), (30, 15, 30), (15, 30, 15), (30, 30, 30)],
     'activation': ['tanh', 'relu', 'logistic', 'identity'],
 }
 
@@ -277,20 +277,21 @@ classifier_pipeline = make_pipeline(preprocessing.MinMaxScaler(),
                                                   random_state=random))
 
 param_grid = {
-    'mlpclassifier__hidden_layer_sizes': [(15,15,15),(30, 15, 30), (15, 30, 15), (30, 30, 30)],
+    'mlpclassifier__hidden_layer_sizes': [(15, 15, 15), (30, 15, 30), (15, 30, 15), (30, 30, 30)],
     'mlpclassifier__activation': ['tanh', 'relu', 'logistic', 'identity'],
 }
 
 mlp1 = GridSearchCV(classifier_pipeline, param_grid, cv=3,
                     n_jobs=-1, verbose=True)
 
-mlp1.fit(X,y)
+mlp1.fit(X, y)
 best_estimator = mlp1.best_estimator_
 
 print('Best Mean Score With Preprocessing', mlp1.best_score_, 'Model', mlp1.best_estimator_)
 mlp1_results = pd.DataFrame(mlp1.cv_results_)
 
-sns.barplot('param_mlpclassifier__hidden_layer_sizes', 'mean_test_score', 'param_mlpclassifier__activation', data=mlp1_results)
+sns.barplot('param_mlpclassifier__hidden_layer_sizes', 'mean_test_score', 'param_mlpclassifier__activation',
+            data=mlp1_results)
 plt.savefig("plots/mlp_p_comparision.png")
 plt.show()
 
@@ -318,21 +319,42 @@ best_estimator.fit(X_train, y_train)
 print('Best Score Hold Out', best_estimator.score(X_test, y_test))
 
 # %% Kaggle Score 0.97647
-#X_scale = min_max_scaler.fit_transform(X)
-#test = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.tes.csv').drop('ID', axis=1)
-#test.columns = test.columns.str.strip()
-#sol = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.sol.ex.csv')
+# X_scale = min_max_scaler.fit_transform(X)
+# test = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.tes.csv').drop('ID', axis=1)
+# test.columns = test.columns.str.strip()
+# sol = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.sol.ex.csv')
 
-#mlp = MLPClassifier(
+# mlp = MLPClassifier(
 #    hidden_layer_sizes=(30,15,30),
 #    max_iter=3000,
 #    alpha=0.001,
 #    activation='relu',
 #    solver='adam',
-#)
+# )
 
-#mlp.fit(X_scale, y)
-#prediction = mlp.predict(min_max_scaler.transform(test))
+# mlp.fit(X_scale, y)
+# prediction = mlp.predict(min_max_scaler.transform(test))
 
-#sol['class'] = prediction
-#sol.to_csv("breast/dataset/sol.csv", index=False)
+# sol['class'] = prediction
+# sol.to_csv("breast/dataset/sol.csv", index=False)
+
+# %% Kaggle knn
+min_max_scaler=MinMaxScaler()
+X_scale = min_max_scaler.fit_transform(X)
+test = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.tes.csv').drop('ID', axis=1)
+test.columns = test.columns.str.strip()
+sol = pd.read_csv('breast/dataset/breast-cancer-diagnostic.shuf.sol.ex.csv')
+
+knn = KNeighborsClassifier(
+    leaf_size=30,
+    n_neighbors=10,
+    p=2,
+    weights='distance',
+    metric='euclidean'
+)
+
+knn.fit(X_scale, y)
+prediction = knn.predict(min_max_scaler.transform(test))
+
+sol['class'] = prediction
+sol.to_csv("breast/dataset/sol.csv", index=False)
